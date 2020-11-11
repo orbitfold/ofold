e27ceb7d80745b05643b37df23ae7e3538c6b7e2
Vytautas Jancauskas <unaudio@gmail.com>
2020-11-11 14:33:53 +0100

    added utils.py

---
+++ src/ofold/utils.py
@@ -0,0 +1,40 @@
+def degree_to_note(root, mode, degree):   
+    """Convert a list of relative degrees to midi note numbers.
+
+    Parameters
+    ----------
+    key - MIDI note number for the root note
+    mode - a number for the mode (0 - Ionian (major), 
+           1 - Dorian, ..., 5 - Aeolian (minor), ...)
+    degree - a scale degree number relative to root (root is 0), can be negative
+
+    Returns
+    -------
+    an integer signifying MIDI note numbers
+    """
+    intervals = [2, 2, 1, 2, 2, 2, 1]
+    intervals = intervals[-mode:] + intervals[mode:]
+    scale = [0]
+    for interval in intervals:
+        scale.append(scale[-1] + interval)
+    root_mod = degree // 7
+    return (root + 12 * root_mod) + scale[degree]
+
+    
+def degrees_to_notes(root, mode, degrees):
+    """Convert a list of relative degrees to midi note numbers.
+
+    Parameters
+    ----------
+    key - MIDI note number for the root note
+    mode - a number for the mode (0 - Ionian (major), 
+           1 - Dorian, ..., 5 - Aeolian (minor), ...)
+    degrees - a list of scale degrees relative to root (root is 0), can be negative
+
+    Returns
+    -------
+    a list of integers signifying MIDI note numbers
+    """
+    assert((0 <= root) and (root <= 127))
+    assert((0 <= mode) and (mode <= 7))
+    return None
