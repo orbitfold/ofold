a32fe383c84e271e35fa1f7d74c1e1d140b0b858
Vytautas Jancauskas <unaudio@gmail.com>
2020-11-13 15:57:47 +0100

    added notes_to_midi

--- src/ofold/utils.py
+++ src/ofold/utils.py
@@ -1,3 +1,5 @@
+from midiutil import MIDIFile
+
 def degree_to_note(root, mode, degree):   
     """Convert a list of relative degrees to midi note numbers.
 
@@ -40,5 +42,27 @@ def degrees_to_notes(root, mode, degrees):
     return [degree_to_note(root, mode, degree) for degree in degrees]
 
 
-def degrees_to_midi(root, mode, degrees, durations, filename):
-    pass
+def notes_to_midi(notes, durations, filename):
+    """Write note values to a MIDI file.
+
+    Parameters
+    ----------
+    notes - a list of MIDI note numbers
+    durations - a list of durations
+    filename - string
+      filename to write to
+    """
+    assert(len(notes) == len(durations))
+    track = 0
+    channel = 0
+    duration = 1
+    tempo = 130
+    volume = 100
+    MyMIDI = MIDIFile(1)
+    MyMIDI.addTempo(track, 0, tempo)
+    position = 0.0
+    for note, duration in zip(notes, durations):
+        MyMIDI.addNote(track, channel, note, position, duration, volume)
+        position += duration
+    with open(filename, "wb") as fd:
+        MyMIDI.writeFile(fd)
