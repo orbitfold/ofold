653a6e830a483d0598da088bd331e5573f48cc73
Vytautas Jancauskas <unaudio@gmail.com>
2022-07-15 13:18:59 +0200

    Update event.py

--- src/ofold/event.py
+++ src/ofold/event.py
@@ -1,4 +1,29 @@
 class Event:
+  """Represents a musical event. Can have a pitch and duration.
+  
+  Parameters
+  ----------
+  pitch: float
+    MIDI note number
+  duration: float
+    duration where 1 - whole note 0.5 - half note and likewise
+  """
   def __init__(self, pitch, duration):
     self.pitch = pitch
     self.duration = duration
+
+def parse_variable_name(name):
+  """Will try to parse an omn style variable name into event list.
+  
+  Parameters
+  ----------
+  name: string
+    note name of the style cs4g4 etc
+     
+  Returns
+  -------
+  A list of Events
+  """
+  state_graph = {
+    "start" : ["note"],
+  }
